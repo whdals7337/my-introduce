384c68c1712da5b186e00849b2bf53f50b31f3fe
wellbell <uok02018496@naver.com>
2020-12-05 20:34:36 +0900

    환경 설정 (롬복, h2 추가)

---
+++ back-end/src/main/java/introduce/Application.java
@@ -0,0 +1,13 @@
+package introduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing // JPA Auditing 활성화 어노테이션
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
