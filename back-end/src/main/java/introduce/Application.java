f7c67dcc3fa8bc52d5c504191d39b3a66d6c19f8
wellbell <uok02018496@naver.com>
2020-12-11 00:28:43 +0900

    차후 MvcTest 를 위한 jpa config 분리

--- back-end/src/main/java/introduce/Application.java
+++ back-end/src/main/java/introduce/Application.java
@@ -2,9 +2,7 @@ package introduce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화 어노테이션
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ back-end/src/main/java/introduce/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package introduce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 활성화 어노테이션
+public class JpaConfig {}
