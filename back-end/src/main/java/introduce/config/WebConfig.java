fc2971ed686920273585a465d2714b93fca01961
wellbell <uok02018496@naver.com>
2020-12-21 20:46:10 +0900

    webConfig allow 주석 제거

--- back-end/src/main/java/introduce/config/WebConfig.java
+++ back-end/src/main/java/introduce/config/WebConfig.java
@@ -9,10 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        /*registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST");*/
-
         registry.addMapping("/**")
                 .allowedOrigins("http://ec2-13-125-104-210.ap-northeast-2.compute.amazonaws.com");
     }
