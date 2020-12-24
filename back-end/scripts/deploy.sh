320192c0c926aa28bca0bdaa2754bc353fa88d9a
wellbell <uok02018496@naver.com>
2020-12-25 05:12:11 +0900

    back-end deploy.sh 수정

--- back-end/scripts/deploy.sh
+++ back-end/scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/back-end-zip/*.jar $REPOSITORY/back-end
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl back-end | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl back-end | grep java | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
