7ed95b510157fd32726b311b2ef74bfbd30e1f34
wellbell <uok02018496@naver.com>
2020-12-31 15:30:31 +0900

    file 다운로드 한글이름 인코딩 로직 추가

---
+++ back-end/src/main/java/introduce/web/FileController.java
@@ -0,0 +1,129 @@
+package introduce.web;
+
+import introduce.domain.member.Member;
+import introduce.domain.project.Project;
+import introduce.domain.skill.Skill;
+import introduce.service.MemberService;
+import introduce.service.ProjectService;
+import introduce.service.SkillService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+
+
+@RequiredArgsConstructor
+@RestController
+public class FileController {
+
+    private final MemberService memberService;
+
+    private final ProjectService projectService;
+
+    private final SkillService skillService;
+
+    @GetMapping("/api/download/{type}/{id}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String type, @PathVariable("id") Long id, HttpServletRequest request) throws Exception {
+        String filePath;
+        String filename;
+
+        switch (type) {
+            case "member" :
+                Member member = memberService.getMember(id);
+                filePath = member.getFilePath();
+                filename = member.getFileOriginName();
+                break;
+            case "project" :
+                Project project = projectService.getProject(id);
+                filePath = project.getFilePath();
+                filename = project.getFileOriginName();
+                break;
+            case "skill" :
+                Skill skill = skillService.getSkill(id);
+                filePath = skill.getFilePath();
+                filename = skill.getFileOriginName();
+                break;
+            default:
+                filePath = null;
+                filename = null;
+        }
+
+        if(filePath != null && filename != null){
+            filename = getFileNameByBrowser(filename, request);
+            Path path = Paths.get(filePath);
+            Resource resource = new InputStreamResource(Files.newInputStream(path));
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                    .body(resource);
+        }
+        else {
+            throw new IOException("존재하지않는 파일입니다.");
+        }
+    }
+
+    protected String getFileNameByBrowser(String fileName, HttpServletRequest request) throws Exception {
+        String browser= "";
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            if(headerName.equals("user-agent")) {
+                browser = request.getHeader(headerName);
+            }
+        }
+
+        String docName = "";
+        if (browser.contains("Trident") || browser.contains("MSIE") || browser.contains("Edge")) {
+            docName = mappingSpecialCharacter(URLEncoder.encode(fileName, "UTF-8"));
+
+        } else if (browser.contains("Firefox")) {
+            docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+
+        } else if (browser.contains("Opera")) {
+            docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+
+        } else if (browser.contains("Chrome")) {
+            docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+
+        } else if (browser.contains("Safari")) {
+            docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+
+        }
+        return docName;
+    }
+
+    public static String mappingSpecialCharacter(String name) {
+
+        // 파일명에 사용되는 특수문자
+        char[] sh_list = { '~', '!', '@', '#', '$', '%', '&', '(', ')', '=', ';', '[', ']', '{', '}', '^', '-' };
+        try {
+            for (char sh : sh_list) {
+                String encodeStr = URLEncoder.encode(sh + "", "UTF-8");
+                name = name.replaceAll(encodeStr, "\\" + sh);
+            }
+
+            // 띄워쓰기 -> + 치환
+            name = name.replaceAll("%2B", "+");
+            // 콤마 -> _ 치환
+            name = name.replaceAll("%2C", "_");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return name;
+    }
+}
