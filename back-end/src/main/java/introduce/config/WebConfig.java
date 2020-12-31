35cf3b82aa9c1ba7dde76eb2eb8299a73906f103
wellbell <uok02018496@naver.com>
2020-12-31 16:55:09 +0900

    cors 설정 변경

--- back-end/src/main/java/introduce/config/WebConfig.java
+++ back-end/src/main/java/introduce/config/WebConfig.java
@@ -19,8 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://ec2-13-125-104-210.ap-northeast-2.compute.amazonaws.com")
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET");
     }
 }
