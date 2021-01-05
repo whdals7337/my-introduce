80270f5142f98cb4606d082c98776eb1c9fedda0
wellbell <uok02018496@naver.com>
2021-01-06 01:28:09 +0900

    테스트 수정

--- back-end/src/test/java/introduce/web/api/MemberApiControllerTest.java
+++ back-end/src/test/java/introduce/web/api/MemberApiControllerTest.java
@@ -321,9 +321,7 @@ public class MemberApiControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value(200))
                 .andExpect(jsonPath("$.data.[0].comment").value("코멘트"))
-                .andExpect(jsonPath("$.data.[1].comment").value("코멘트"))
-                .andExpect(jsonPath("$.pagination.total_elements").value(size))
-                .andExpect(jsonPath("$.pagination.current_elements").value(2));
+                .andExpect(jsonPath("$.data.[1].comment").value("코멘트"));
     }
 
     @Test
